0cfacbe1523bed923fb6509ab4e0a47a521c4534
Ítalo Assunção <italoaa99@gmail.com>
2022-11-23 23:05:26 -0300

    reverting map position

--- scenes/main.py
+++ scenes/main.py
@@ -9,37 +9,37 @@ from scenes.FourthRoom import *
 
 def build_forest():
   forest.reset_transition_spots()
-  forest.add_scene_transition_spot(castle_entry, 'bottom')
+  forest.add_scene_transition_spot(castle_entry, 'top')
 
 def build_castle_entry():
   castle_entry.reset_transition_spots()
-  castle_entry.add_scene_transition_spot(forest, 'top')
-  castle_entry.add_scene_transition_spot(first_room, 'bottom')
+  castle_entry.add_scene_transition_spot(forest, 'bottom')
+  castle_entry.add_scene_transition_spot(first_room, 'top')
 
 def build_first_room():
   castle_entry.reset_transition_spots()
-  first_room.add_scene_transition_spot(castle_entry, 'top')
-  first_room.add_scene_transition_spot(auxiliar_room, 'left')
-  first_room.add_scene_transition_spot(hallway, 'bottom')
+  first_room.add_scene_transition_spot(castle_entry, 'bottom')
+  first_room.add_scene_transition_spot(auxiliar_room, 'right')
+  first_room.add_scene_transition_spot(hallway, 'top')
 
 def build_auxiliar_room():
   auxiliar_room.reset_transition_spots()
-  auxiliar_room.add_scene_transition_spot(first_room, 'right')
+  auxiliar_room.add_scene_transition_spot(first_room, 'left')
 
 def build_hallway():
   hallway.reset_transition_spots()
-  hallway.add_scene_transition_spot(first_room, 'top')
-  hallway.add_scene_transition_spot(second_room, 'bottom')
+  hallway.add_scene_transition_spot(first_room, 'bottom')
+  hallway.add_scene_transition_spot(second_room, 'top')
 
 def build_second_room():
   second_room.reset_transition_spots()
-  second_room.add_scene_transition_spot(hallway, 'top')
-  second_room.add_scene_transition_spot(third_room, 'bottom')
-  second_room.add_scene_transition_spot(fourth_room, 'left')
+  second_room.add_scene_transition_spot(hallway, 'bottom')
+  second_room.add_scene_transition_spot(third_room, 'top')
+  second_room.add_scene_transition_spot(fourth_room, 'right')
 
 def build_third_room():
   third_room.reset_transition_spots()
-  third_room.add_scene_transition_spot(second_room, 'top')
+  third_room.add_scene_transition_spot(second_room, 'bottom')
 
 
 def build_fourth_room():  
